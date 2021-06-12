3892d31c4f6db67ea8e153f0c159764a5ba3063f
Nicolas Alderete <nicolasalderete@gmail.com>
2021-06-11 21:44:58 -0300

    modifico la consulta de ofertas destacadas

--- inc/menu.php
+++ inc/menu.php
@@ -18,8 +18,8 @@
     <?php endif; ?>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <a class="navbar-brand" href="/php-cuch">Dietetica</a>
-        <bun1tton class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="/php-cuch">Riestra Natural</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         
