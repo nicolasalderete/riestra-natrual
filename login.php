eb8a19efe9ec8be9f75525db7b2e3d291592c757
Nicolas Alderete <nicolasalderete@gmail.com>
2021-06-11 23:18:50 -0300

    agrego cambios

--- login.php
+++ login.php
@@ -164,7 +164,7 @@ body {
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
           <div class="card-body">
-            <h5 class="card-title text-center">Sign In</h5>
+            <h5 class="card-title text-center">Iniciar sesión</h5>
             <form class="form-signin">
               <div class="form-label-group">
                 <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
@@ -176,14 +176,7 @@ body {
                 <label for="inputPassword">Password</label>
               </div>
 
-              <div class="custom-control custom-checkbox mb-3">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Remember password</label>
-              </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-              <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Entrar</button>
             </form>
           </div>
         </div>
